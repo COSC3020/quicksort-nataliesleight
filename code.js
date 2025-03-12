bf8b6ef7fa7e45273a5accb8af1245303352343d
nataliesleight <nsleight@uwyo.edu>
2025-03-12 13:21:55 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,34 +1,53 @@
-function quicksort(array) {
-    var n = array.length;
-    if (n == 0 || n == 1) {return array;}
-    qSort(array, 0, n - 1);
-    return array;
-}
+function quicksort(array)
+    {   
+        if (array.length == 0 || array.length == 1) {return array;}
 
-function qSort(arr, low, high) {
-    if (low < high) {
-        var pi = partition(arr, low, high);
-        qSort(arr, low, pi - 1);
-        qSort(arr, pi + 1, high);
-    }
-}
+        var low = 0;
+        var high = array.length -1;
 
-function partition(arr, low, high) {
-    var temp;
-    var pivot = arr[high];
-
-    var i = (low - 1);
-    for (var j = low; j <= high - 1; j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        var stack = new Array(high - low + 1);
+        stack.fill(0);
+        var top = -1;
+        stack[++top] = low;
+        stack[++top] = high;
+ 
+        while (top >= 0) {
+            high = stack[top--];
+            low = stack[top--];
+ 
+            var pivot = partition(array, low, high);
+ 
+            if (pivot - 1 > low) {
+                stack[++top] = low;
+                stack[++top] = p - 1;
+            }
+ 
+            if (pivot + 1 < high) {
+                stack[++top] = pivot + 1;
+                stack[++top] = high;
+            }
         }
+        return array;
     }
 
-    temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    return i + 1;
-}
+function partition(arr, low, high)
+    {
+        var temp;
+        var pivot = arr[high];
+        var i = (low - 1);
+
+        for (var j = low; j <= high - 1; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+ 
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+ 
+        return i + 1;
+    }
